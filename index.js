0ab4f29a10c6c2533c13414f3bbadd12917d1a0f
Azinudin Achzab <azinudinachzab@users.noreply.github.com>
2024-09-11 15:20:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ var server = createServer(function (req, res) {
 });
 
 // starts a simple http server locally on port 3000
-server.listen(process.env.PORT || '3000', 'localhost', function () {
-  console.log('Listening on localhost:3000');
+const PORT = process.env.PORT || 8080
+server.listen(PORT, '127.0.0.1', function () {
+  console.log(`Listening on localhost:${PORT}`);
 });
 
 // run with `node server.mjs`
